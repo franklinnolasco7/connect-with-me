8f58fa19245819e2f8d454bcdc3f19366b9f0ef3
Frank <nolascofranklin09@gmail.com>
2026-07-19 21:56:46 +0800

    fix: link DEV profile

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -65,7 +65,7 @@ export const profile = {
     {
       id: "devto",
       label: "dev.to",
-      href: "https://dev.to",
+      href: "https://dev.to/franklinnolasco7",
       icon: "devto",
       external: true,
     },
